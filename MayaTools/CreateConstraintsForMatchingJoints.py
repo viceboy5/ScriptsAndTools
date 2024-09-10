2f03db6ce106783a4440222877b25c96ff574303
Hank <10811528@my.uvu.edu>
2024-09-09 21:48:37 -0600

    updates

--- MayaTools/CreateConstraintsForMatchingJoints.py
+++ MayaTools/CreateConstraintsForMatchingJoints.py
@@ -1,8 +1,9 @@
 import maya.cmds as cmds
 
-def find_matching_joint(curve_name):
-    # Extract joint name from the curve name
-    joint_name = curve_name.replace("_Ctrl", "")
+
+def find_matching_joint(ctrl_name):
+    # Extract joint name from the control name
+    joint_name = ctrl_name.replace("_Ctrl", "")
 
     # Check if the joint exists
     if cmds.objExists(joint_name):
@@ -10,30 +11,33 @@ def find_matching_joint(curve_name):
     else:
         return None
 
-def create_constraints(curve_name, joint_name):
+
+def create_constraints(ctrl_name, joint_name):
     # Create parent constraint
-    parent_constraint = cmds.parentConstraint(curve_name, joint_name, mo=True)[0]
+    parent_constraint = cmds.parentConstraint(ctrl_name, joint_name, mo=True)[0]
 
     # Create scale constraint
-    scale_constraint = cmds.scaleConstraint(curve_name, joint_name, mo=True)[0]
+    scale_constraint = cmds.scaleConstraint(ctrl_name, joint_name, mo=True)[0]
 
     return parent_constraint, scale_constraint
 
-def find_and_create_constraints():
-    # List all curves in the scene
-    curves = cmds.ls(type='nurbsCurve')
 
-    for curve in curves:
-        # Check if the curve name ends with '_Ctrl'
-        if curve.endswith('_Ctrl'):
+def find_and_create_constraints():
+    # List all transform nodes in the scene
+    transforms = cmds.ls(type='transform')
+
+    for ctrl in transforms:
+        # Check if the control name ends with '_Ctrl'
+        if ctrl.endswith('_Ctrl'):
             # Find matching joint
-            joint_name = find_matching_joint(curve)
+            joint_name = find_matching_joint(ctrl)
 
             if joint_name:
                 # Create constraints
-                create_constraints(curve, joint_name)
+                create_constraints(ctrl, joint_name)
             else:
-                print(joint_name)
+                print(f"Joint not found for control: {ctrl}")
+
 
 # Call the function to find and create constraints
 find_and_create_constraints()
