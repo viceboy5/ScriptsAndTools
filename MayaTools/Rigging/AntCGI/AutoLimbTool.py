a877d662e41f3a6135f48590cebf02f463cc38bf
Hank <10811528@my.uvu.edu>
2025-07-01 15:00:21 -0600

    tool start

---
+++ MayaTools/Rigging/AntCGI/AutoLimbTool.py
@@ -0,0 +1,9 @@
+#-----------------------------------------------
+#
+#antCGI Auto Limb Tool V1, created from part 21 antCGI video tutorial
+#
+#
+#-----------------------------------------------
+
+import maya.cmds as cmds
+
